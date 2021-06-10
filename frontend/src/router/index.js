6fadd45c908bd8471bb40686b4ead156b5a8ee5c
Mike Olund <mike@oxd.com>
2021-06-10 14:02:01 -0700

    Remove the About page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,15 +7,6 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    meta: { authenticated: true }
-  },
-  {
     path: '/',
     name: 'Home',
     component: Home,
