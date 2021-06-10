1e4915320dafa67c5b5ff4692acc60d998c29b1e
Mike Olund <mike@oxd.com>
2021-06-10 14:02:38 -0700

    Change from history mode router to default mode

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,13 +10,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
-    alias: '/index.html',
+    alias: ['/state*', ''],
     meta: { authenticated: true }
   },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
@@ -33,20 +32,13 @@ const authGuard = async (to, from, next) => {
 
   if (!store.getters["user/isAuthenticated"]) {
     await Vue.$keycloak.login({
-      redirectUri: `${window.location.origin}${to.fullPath}`
+      redirectUri: `${window.location.origin}/index.html`
     });
   }
 
   next();
 };
 
-router.onReady(() => { 
-  // Electron messes up the app:// url after 302 redirect from Keycloak
-  if (location.href.startsWith("app://./app:/")) {
-    const tidyHref = location.href.replace("app://./app:/", "app://./");
-    history.replaceState(window.history.state, null, tidyHref);
-  }
-})
 
 router.beforeEach(authGuard);
 
